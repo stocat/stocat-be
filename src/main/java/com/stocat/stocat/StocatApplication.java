6b7b251db8a9dded7fb1b056f265be06883d4c2f
jjh5887 <wjdrnjsgh159@gmail.com>
2025-06-15 22:20:13 +0900

    chore: 단일 구조로 변경

---
+++ src/main/java/com/stocat/stocat/StocatApplication.java
@@ -0,0 +1,13 @@
+package com.stocat.stocat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StocatApplication.class, args);
+    }
+
+}
