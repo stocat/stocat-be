dc660fbf825bde7ee1974d7d016955f072f946c9
xEzIxX <by1094@gachon.ac.kr>
2025-11-29 17:30:00 +0900

    refactor: 매매 구분 메서드 가독성 개선

--- asset-scraper/src/main/java/com/stocat/asset/scraper/crypto/messaging/event/TradeSide.java
+++ asset-scraper/src/main/java/com/stocat/asset/scraper/crypto/messaging/event/TradeSide.java
@@ -7,7 +7,13 @@ public enum TradeSide {
     BUY,
     SELL;
 
-    public static TradeSide fromUpbitAb(String ab) {
-        return "ASK".equalsIgnoreCase(ab) ? SELL : BUY;
+    public static TradeSide fromUpbitSide(String side) {
+        if (side.equals("ASK")){
+            return SELL;
+        } else if (side.equals("BID")) {
+            return BUY;
+        }
+        // TODO: 에러 핸들링
+        throw new IllegalArgumentException();
     }
 }
