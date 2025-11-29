88c91c0e5543cdb69de4176f70c9fd164963b181
xEzIxX <by1094@gachon.ac.kr>
2025-11-30 01:32:56 +0900

    refactor: UpbitCryptoMarketService 상수 및 메서드 분리

--- asset-scraper/src/main/java/com/stocat/asset/scraper/crypto/service/UpbitCryptoMarketService.java
+++ asset-scraper/src/main/java/com/stocat/asset/scraper/crypto/service/UpbitCryptoMarketService.java
@@ -19,6 +19,8 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class UpbitCryptoMarketService {
 
+    private static final String TRADING_VOLUME_SOARING = "TRADING_VOLUME_SOARING";
+
     private final WebClient upbitWebClient;
 
     public Set<MarketInfo> getTopKrwTradeCrypto(int n) {
@@ -32,17 +34,19 @@ public class UpbitCryptoMarketService {
                         .collectList()
                         .block())
                 .stream()
-                .filter(detail ->
-                        detail.marketEvent() != null &&
-                                detail.marketEvent().caution() != null &&
-                                Boolean.TRUE.equals(
-                                        detail.marketEvent().caution().get("TRADING_VOLUME_SOARING")
-                                )
-                )
+                .filter(UpbitCryptoMarketService::isTradingVolumeSoaring)
                 .filter(detail -> detail.market().startsWith("KRW-"))
                 .limit(n)
                 .map(detail -> new MarketInfo(detail.market(), detail.koreanName(), detail.englishName()))
                 .collect(Collectors.toSet());
+    }
+
+    private static boolean isTradingVolumeSoaring(MarketEventDetailResponse detail) {
+        return detail.marketEvent() != null &&
+                detail.marketEvent().caution() != null &&
+                Boolean.TRUE.equals(
+                        detail.marketEvent().caution().get(TRADING_VOLUME_SOARING)
+                );
     }
 
     /**
@@ -124,7 +128,7 @@ public class UpbitCryptoMarketService {
     private List<MarketEventDetailResponse> getVolumeSoaring(Collection<MarketEventDetailResponse> allDetails) {
         return allDetails.stream()
                 .filter(md -> md.marketEvent() != null && md.marketEvent().caution() != null)
-                .filter(md -> Boolean.TRUE.equals(md.marketEvent().caution().get("TRADING_VOLUME_SOARING")))
+                .filter(md -> Boolean.TRUE.equals(md.marketEvent().caution().get(TRADING_VOLUME_SOARING)))
                 .distinct()
                 .toList();
     }
