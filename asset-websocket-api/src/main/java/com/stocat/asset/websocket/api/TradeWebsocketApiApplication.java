f8ca3bcc585053866ff88301eebf464da0a5df55
xEzIxX <by1094@gachon.ac.kr>
2025-12-07 23:49:04 +0900

    refactor: asset-websocket-api 패키지명 변경에 따른 수정 사항 반영

--- asset-websocket-api/src/main/java/com/stocat/asset/websocket/api/TradeWebsocketApiApplication.java
+++ asset-websocket-api/src/main/java/com/stocat/asset/websocket/api/TradeWebsocketApiApplication.java
@@ -7,8 +7,8 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 @EnableWebFlux
 @SpringBootApplication(
         scanBasePackages = {
-                "com.stocat.trade.websocket.api",
-                "com.stocat.common.redis"
+                "com.stocat.asset.websocket.api",
+                "com.stocat.common.redis",
         }
 )
 public class TradeWebsocketApiApplication {
